1139ae26b5bcac4865e3b13bf590cf38c6dcb4ba
Jason Díaz <jason.diaz.mp@gmail.com>
2017-08-10 16:50:42 -0600

    Change in jason branch

--- Hola.java
+++ Hola.java
@@ -1,6 +1,6 @@
 public class Hola {
 	public static void main(String args[]){
 		System.out.println("Hola mundo");
-		System.out.println("Este es el curso de git");
+		System.out.println("Este es el curso de git. Modificación con jason");
 	}
 }
